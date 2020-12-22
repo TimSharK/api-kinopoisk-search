63bf1c3de202d8f54e57847f2b44b67d23ca77fb
TimSharK <tgimranov@gmail.com>
2020-12-23 00:27:22 +0300

    done

--- api/movies.js
+++ api/movies.js
@@ -8,15 +8,14 @@ async function search({ term, page }) {
         };
 
     let movies = [];
-    let maxPages = 1;
+    let maxPages = 0;
     let currentPage = 1;
 
     while(true) {
         if (currentPage == maxPages)
             break;
 
-        let url = `https://api.kinopoisk.cloud/movies/all/page/${page}/token/26ae258e5b4382bd838cf3cf79c8a1dc`;
-        let response = (await axios.get(url.toString()));
+        var response = getData(currentPage);
 
         if (response.status !== 200)
             return response.data;
@@ -25,9 +24,9 @@ async function search({ term, page }) {
             return movie.title.toLowerCase().indexOf(term.toLowerCase()) != -1;
         })
 
-        movies.push(pageMovies);
+        movies.push(...pageMovies);
 
-        maxPages = response.data.maxPages;
+        maxPages = response.data.pagination.total_pages;
         currentPage++;
     }
 
@@ -35,6 +34,31 @@ async function search({ term, page }) {
         items: movies,
         maxPages: maxPages
     };
+}
+
+function getData(page) {
+    //let url = `https://api.kinopoisk.cloud/movies/all/page/${page}/token/26ae258e5b4382bd838cf3cf79c8a1dc`;
+    //let response = (await axios.get(url.toString()));
+    //return response;
+
+    return {
+        data: {
+            movies: [{
+                title: "HZ",
+                actors: ["1", "2"]
+            },{
+                title: "HZ2",
+                actors: ["3", "4"]
+            },{
+                title: "HZ3",
+                actors: ["4", "5"]
+            }],
+            pagination: {
+                total_pages: 5
+            }
+        },
+        status: 200
+    }
 }
   
 export { search };
